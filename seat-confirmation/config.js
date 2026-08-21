7fd537209684553240874a183c99b3509a30ab7d
ISHA-TAICHUNG <h048610640002@gmail.com>
2026-08-21 15:52:07 +0800

    chore: 更新本次考試網址

--- seat-confirmation/config.js
+++ seat-confirmation/config.js
@@ -7,7 +7,7 @@ window.SEAT_APP_CONFIG = Object.freeze({
   siteTitle: "個人座號查詢",
   apiUrl: "https://script.google.com/macros/s/AKfycbzzwCzzAgFJOuRRw1SjYpAn8wfaAw1txInqzq6JBxm06ddlme3qPzASy4Ilft9g3fK2/exec",
   requestTimeoutMs: 12000,
-  quizUrl: "https://oshcard.osha.gov.tw/onlineQuiz/Login?trId=2xKy%2FK9yn513HBipeEQRvg%3D%3D",
+  quizUrl: "https://oshcard.osha.gov.tw/onlineQuiz/Login?trId=RqRwbi6APiTP9wg8ODMAYQ%3D%3D",
   quizTimeZone: "Asia/Taipei",
   quizAvailableHour: 15,
 });
