bf27ddc5e7c99e54d66479935d016e6fcc0dc9c2
ISHA-TAICHUNG <h048610640002@gmail.com>
2026-08-22 10:55:53 +0800

    設定：改用第 7 版 Apps Script API

--- seat-confirmation/config.js
+++ seat-confirmation/config.js
@@ -1,13 +1,10 @@
 /*
- * 只需要修改 apiUrl。
+ * 只需要修改 apiUrl；考試網址由試算表的「網站設定」分頁提供。
  * 這個網址是 Google Apps Script Web App 的 /exec 網址；它不是試算表網址。
  * 不要把 Google 試算表匯出成 CSV、JSON 或其他檔案放進網站資料夾。
  */
 window.SEAT_APP_CONFIG = Object.freeze({
   siteTitle: "個人座號查詢",
-  apiUrl: "https://script.google.com/macros/s/AKfycbzzwCzzAgFJOuRRw1SjYpAn8wfaAw1txInqzq6JBxm06ddlme3qPzASy4Ilft9g3fK2/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzNHw2_eS2oNcIWlgpOLRKlBH59TGQOIFowKsYTAXwHJITS461vGa6IuMW9hEcNsZM/exec",
   requestTimeoutMs: 12000,
-  quizUrl: "https://oshcard.osha.gov.tw/onlineQuiz/Login?trId=RqRwbi6APiTP9wg8ODMAYQ%3D%3D",
-  quizTimeZone: "Asia/Taipei",
-  quizAvailableHour: 15,
 });
